c83a2cd5587999432215a6131dac750a9c14a361
v-prusakov <v-prusakov@yandex-team.ru>
2017-07-27 01:47:06 +0300

    fullscreen image mode added

---
+++ app/components/image-modal/image-modal.vue
@@ -0,0 +1,58 @@
+<template>
+	<div class="image-modal" v-on:click="close" v-bind:class="mod">
+		<div class="image-modal__image" v-bind:style="imagePositionData">
+			<img v-bind:src='imageData.src'></div>
+		</div>
+	</div>
+</template>
+
+<script>
+import './image-modal.sass';
+
+export default {
+	data () {
+		return {};
+	},
+	props: ['imageData'],
+	computed: {
+		isActive () {
+			return this.imageData.active;
+		},
+		imagePositionData () {
+			const offsetData = this.imageData.offsetData;
+
+			if (!offsetData) {
+				return {};
+			}
+
+			return {
+				width: `${offsetData.width}px`,
+				top: `${offsetData.top}px`,
+				left: `${offsetData.left}px`
+			};
+		},
+		mod () {
+			return {
+				'image-modal_active': this.isActive
+			}
+		}
+	},
+	mounted () {
+		document.addEventListener('keyup', (event) => {
+			if (event.keyCode == 27) {
+				this.close();
+			}
+		});
+	},
+	methods: {
+		close () {
+			this.imageData.active = false;
+		}
+	},
+	watch: {
+		isActive () {
+			// alert();
+		}
+	}
+}
+</script>
--- app/components/product/product.vue
+++ app/components/product/product.vue
@@ -6,7 +6,7 @@
 			<div class="product__image"><img v-bind:src='table'></div>
 			<div class="product__section product__section_1">
 				<div class="product__section_left">
-					<img v-bind:src='table'>
+					<div class="product__inner-image" v-on:click="showFullImage($event, table)"><img v-bind:src='table'></div>
 					<div class="product__text-block">
 						<h2 class="product__text-heading">Lorem ipsum dolor sit.</h2>
 						<p class="product__text-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit accusamus reiciendis quas nemo qui perferendis provident, laborum impedit velit voluptates corporis quam tempora, deleniti distinctio vitae nisi neque repellendus amet!
@@ -14,15 +14,15 @@
 					</div>
 				</div>
 				<div class="product__section_right">
-					<img v-bind:src='table'>
+					<div class="product__inner-image" v-on:click="showFullImage($event, table)"><img v-bind:src='table'></div>
 				</div>
 			</div>
 			<div class="product__section product__section_2">
 				<div class="product__section_left">
-					<img v-bind:src='table'>
+					<div class="product__inner-image" v-on:click="showFullImage($event, table)"><img v-bind:src='table'></div>
 				</div>
 				<div class="product__section_right">
-					<img v-bind:src='table'>
+					<div class="product__inner-image" v-on:click="showFullImage($event, table)"><img v-bind:src='table'></div>
 					<div class="product__text-block">
 						<h2 class="product__text-heading">Lorem ipsum dolor sit.</h2>
 						<p class="product__text-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit accusamus reiciendis quas nemo qui perferendis provident, laborum impedit velit voluptates corporis quam tempora, deleniti distinctio vitae nisi neque repellendus amet!</p>
@@ -33,6 +33,7 @@
 				<form-component></form-component>
 			</div>
 		</div>
+		<image-modal v-bind:image-data='imageController'></image-modal>
 	</div>
 </template>
 
@@ -42,18 +43,55 @@ import table from '../index/table.png';
 
 import Nav from '../nav/nav.vue';
 import Form from '../form/form.vue';
+import ImageModal from '../image-modal/image-modal.vue';
 
 export default {
 	data () {
 		return {
 			table: table,
+			recentImage: null,
+			imageController: {
+				active: false,
+				src: null,
+				offsetData: null
+			},
 			items: [
 			]
 		}
 	},
+	methods: {
+		showFullImage (event, image) {
+			const target = event.target;
+			const targetOffsets = target.getBoundingClientRect();
+
+			target.style.opacity = 0;
+
+			this.imageController.offsetData = {
+				top: targetOffsets.top,
+				left: targetOffsets.left,
+				width: targetOffsets.width
+			};
+			this.imageController.src = image;
+			this.imageController.active = true;
+			this.recentImage = target;
+		}
+	},
+	computed: {
+		isClosed () {
+			return !this.imageController.active;
+		}
+	},
+	watch: {
+		isClosed (closed) {
+			if (closed) {
+				this.recentImage.style.opacity = 1;
+			}
+		}
+	},
 	components: {
 		'header-nav': Nav,
-		'form-component': Form
+		'form-component': Form,
+		'image-modal': ImageModal
 	}
 }
 </script>
