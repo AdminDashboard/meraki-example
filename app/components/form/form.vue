c538b056ffa6a4585cbee10923fec6426bf786d5
v-prusakov <v-prusakov@yandex-team.ru>
2017-07-30 17:20:51 +0300

    form validation & confirmatiom, closes #17

--- app/components/form/form.vue
+++ app/components/form/form.vue
@@ -1,6 +1,6 @@
 <template>
-	<div class="form-wrapper">
-		<form action="" class="form">
+	<div class="form-wrapper" v-bind:class="wrapperMod">
+		<form class="form">
 			<div class="form__type-fields">
 				<label class="form__type-group">
 					<input v-model="type" type="radio" value="order" name="type">
@@ -12,39 +12,44 @@
 				</label>
 			</div>
 			<div class="form__fields">
-				<label class="form__group form__group_name">
+				<label class="form__group form__group_name" v-bind:class="{'form__group_invalid': !name}">
 					<input required type="text" v-model="name" class="form__input" v-bind:class="{'form__input_has_value': name}" name="name">
 					<span class="form__label">Name</span>
 				</label>
-				<label class="form__group form__group_email">
+				<label class="form__group form__group_email" v-bind:class="{'form__group_invalid': !email}">
 					<input required type="email" v-model="email" class="form__input" v-bind:class="{'form__input_has_value': email}" name="email">
 					<span class="form__label">Email</span>
 				</label>
-				<label class="form__group form__group_phone">
+				<label class="form__group form__group_phone" v-bind:class="{'form__group_invalid': !phone}">
 					<input required type="phone" v-model="phone" class="form__input" v-bind:class="{'form__input_has_value': phone}" name="phone">
 					<span class="form__label">Phone</span>
 				</label>
-				<label class="form__group form__group_message">
+				<label class="form__group form__group_message" v-bind:class="{'form__group_invalid': !message}">
 					<input required type="text" v-model="message" class="form__input" v-bind:class="{'form__input_has_value': message}" name="message">
 					<span class="form__label">Message</span>
 				</label>
 			</div>
 			<div class="form__check-buttons">
-				<label class="form__check-group">
+				<label class="form__check-group" v-bind:class="{'form__group_check-invalid': !processing}">
 					<span class="form__check-label">i accept the processing of personal data</span>
 					<span class="form__check">
-						<input required type="checkbox" name="processing">
+						<input required v-model="processing" type="checkbox" name="processing">
 					</span>
 				</label>
-				<label class="form__check-group">
+				<label class="form__check-group" v-bind:class="{'form__group_check-invalid': !human}">
 					<span class="form__check-label">i'm human</span>
 					<span class="form__check">
-						<input required type="checkbox" name="human">
+						<input required v-model="human" type="checkbox" name="human">
 					</span>
 				</label>
 			</div>
-			<button type="submit" v-on:click="submit" class="form__submit">Send</button>
+			<button type="submit" v-on:click.stop="submit" class="form__submit">Send</button>
 		</form>
+		<div class="form__dialog">
+			<div class="form__message">
+				Your message has been successfuly sent
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -58,12 +63,51 @@ export default {
 			name: null,
 			email: null,
 			phone: null,
-			message: null
+			message: null,
+			human: null,
+			processing: null,
+			invalidationCheck: false,
+			sentMod: false
+		}
+	},
+	computed: {
+		isValid() {
+			return this.name
+				&& this.email
+				&& this.phone
+				&& this.message
+				&& this.human
+				&& this.processing;
+		},
+		wrapperMod () {
+			return {
+				'form-wrapper_check': this.invalidationCheck,
+				'form-wrapper_sent': this.sentMod
+			}
 		}
 	},
 	methods: {
+		clearData () {
+			this.name
+				= this.email
+				= this.phone
+				= this.message
+				= this.human
+				= this.processing
+				= null;
+		},
 		submit (e) {
-			// e.preventDefault();
+			e.preventDefault();
+			this.invalidationCheck = true;
+
+			if (this.isValid) {
+				this.sentMod = true;
+				setTimeout(() => {
+					this.sentMod = false;
+					this.invalidationCheck = false;
+					this.clearData();
+				}, 2000);
+			}
 		}
 	}
 }
