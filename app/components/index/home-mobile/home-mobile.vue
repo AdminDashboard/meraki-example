88f82ee97f77c6a7fcde1f09050d04de64ed1df4
v-prusakov <v-prusakov@yandex-team.ru>
2017-09-02 11:31:03 +0300

    mobile detection

---
+++ app/components/index/home-mobile/home-mobile.vue
@@ -0,0 +1,75 @@
+<template>
+	<div class='mobileHome'>
+		<header-nav></header-nav>
+		<h1>Mobile</h1>
+	</div>
+</template>
+
+<script>
+import tableImage from '../table.png';
+import aboutLogo from '../logo-2.png';
+import db from '../../database-controller/database-controller.js';
+
+import Nav from '../../nav/nav.vue';
+import ComingSoon from '../coming-soon/coming-soon.vue';
+
+export default {
+	firebase ()  {
+		return {
+			categories: db.ref('parentCat'),
+			subCats: db.ref('subCat')
+		}
+	},
+	data () {
+		return {
+			tableImage: tableImage,
+			aboutLogo: aboutLogo,
+			navItems: [
+				'seating',
+				'beds',
+				'decor',
+				'lighting',
+				'kitchen',
+				'bathroom',
+				'about',
+				'tables'
+			]
+		}
+	},
+	mounted () {
+
+	},
+	computed: {
+		tableCat () {
+			return this.getCategory('tables');
+		},
+		seatingCat () {
+			return this.getCategory('seating');
+		},
+		bedsCat () {
+			return this.getCategory('beds');
+		},
+		decorCat () {
+			return this.getCategory('decor');
+		},
+		lightingCat () {
+			return this.getCategory('lighting');
+		},
+		kitchenCat () {
+			return this.getCategory('kitchen');
+		},
+		bathroomCat () {
+			return this.getCategory('bathroom');
+		}
+	},
+	methods: {
+
+	},
+	components: {
+		'header-nav': Nav,
+		'coming-soon': ComingSoon
+	}
+}
+</script>
+
+<style scoped lang='sass'></style>
--- app/components/index/index.js
+++ app/components/index/index.js
@@ -4,6 +4,7 @@ import VueFire from 'vuefire';
 import VueRouter from 'vue-router';
 import $ from 'jquery';
 import Home from './home.vue';
+import MobileHome from './home-mobile/home-mobile.vue';
 import Products from '../products/products.vue';
 import Product from '../product/product.vue';
 import Admin from '../admin/admin.vue';
@@ -12,10 +13,16 @@ import './fonts/fonts.sass';
 
 window.$ = window.jQuery = $;
 
-const NotFound = { template: '<p>Page not found</p>' };
+let isMobile = false;
+
+if (/Android|webOS|iPhone|iPod|iPad|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+	isMobile = true;
+}
+
+const NotFound = {template: '<p>Page not found</p>'};
 
 const routes = [
-  {path: '/', component: Home},
+  {path: '/', component: isMobile ? MobileHome : Home},
   {path: '/products', component: Products, props: {depth: null}},
   {path: '/products/:cat', component: Products, props: {depth: 'super'}},
   {path: '/products/:cat/items', component: Products, props: {depth: 'products'}},
