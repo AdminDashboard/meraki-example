2c047cd38da32beb5b780861365a93a9c6027b18
v-prusakov <v-prusakov@yandex-team.ru>
2017-09-06 00:21:23 +0300

    mobile home

--- app/components/index/home-mobile/home-mobile.vue
+++ app/components/index/home-mobile/home-mobile.vue
@@ -1,14 +1,43 @@
 <template>
-	<div class='mobileHome'>
-		<h1>Mobile</h1>
+	<div class='mobile-home'>
+		<header-nav v-bind:d-style="'black'"></header-nav>
+		<div class="mobile-home__image"
+			:style="{
+				backgroundImage: `url(${fingerprintImage})`,
+				backgroundPosition: bgPos
+			}"></div>
 		<div class="owl-carousel owl-theme">
-			<div> Your Content1 </div>
-			<div> Your Content </div>
-			<div> Your Content2 </div>
-			<div> Your Content </div>
-			<div> Your Content </div>
-			<div> Your Content </div>
-			<div> Your Content </div>
+			<div class="owl-carousel__item">
+				<div class="mobile-home-main">
+					<div class="mobile-home-main__image">
+						<img :src="aboutLogo" alt="">
+					</div>
+					<div class="mobile-home-main__description">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit ipsum laborum sunt, deleniti odit fugiat totam debitis, nobis aut in, fuga illum ab! Architecto, blanditiis placeat enim deleniti accusamus quibusdam at eum, obcaecati odio expedita excepturi adipisci, molestiae aliquam temporibus aliquid sed vitae rem sapiente et minus! Libero possimus, ipsam?
+					</div>
+				</div>
+			</div>
+			<div class="owl-carousel__item">
+				<div class="mobile-home-main">
+					<div class="mobile-home-main__image">
+						<img :src="aboutLogo" alt="">
+					</div>
+					<div class="mobile-home-main__text">
+						<span class="mobile-home-main__text_main">swipe</span>
+						<span class="mobile-home-main__text_sub">to explore</span>
+					</div>
+				</div>
+			</div>
+			<div class="owl-carousel__item">
+				<div class="mobile-home-products">
+					<a :href="`#/products/${cat.id}`" class="mobile-home-product" v-for="cat in categories">
+						<div class="mobile-home-product__title">{{cat.id}}</div>
+						<div class="mobile-home-product__image">
+							<img :src="cat.mainImage" alt="">
+						</div>
+					</a>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
@@ -16,11 +45,12 @@
 <script>
 import tableImage from '../table.png';
 import aboutLogo from '../logo-2.png';
+import fingerprint from '../fingerprint.png';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'imports-loader?jQuery=jquery!owl.carousel';
 import db from '../../database-controller/database-controller.js';
 
-import Nav from '../../nav/nav.vue';
+import Dropdown from '../../nav/dropdown-menu/dropdown-menu.vue';
 import ComingSoon from '../coming-soon/coming-soon.vue';
 
 export default {
@@ -33,7 +63,9 @@ export default {
 	data () {
 		return {
 			tableImage: tableImage,
+			fingerprintImage: fingerprint,
 			aboutLogo: aboutLogo,
+			bgPos: '50%',
 			navItems: [
 				'seating',
 				'beds',
@@ -47,7 +79,26 @@ export default {
 		}
 	},
 	mounted () {
-		$('.owl-carousel').owlCarousel();
+		const carousel = $('.owl-carousel').owlCarousel({
+			items: 1,
+			startPosition: 1
+		});
+
+		carousel.on('changed.owl.carousel', event => {
+			const index = event.item.index;
+
+			if (index === 1) {
+				this.bgPos = '50%';
+			}
+
+			if (index === 0) {
+				this.bgPos = '200px 50%';
+			}
+
+			if (index === 2) {
+				this.bgPos = '-200px 50%';
+			}
+		});
 	},
 	computed: {
 		tableCat () {
@@ -75,13 +126,96 @@ export default {
 	methods: {
 	},
 	components: {
-		'header-nav': Nav,
+		'header-nav': Dropdown,
 		'coming-soon': ComingSoon
 	}
 }
 </script>
 
 <style scoped lang='sass'>
+	.mobile-home
+		position: relative
+		&__image
+			content: ''
+			position: absolute
+			top: 0
+			right: 0
+			bottom: 0
+			left: 0
+			transition: background .5s ease-in-out
+			opacity: .3
+			background-size: 110%
+			background-repeat: no-repeat
 	.owl-carousel
 		touch-action: manipulation
+	.dropdown-menu
+		position: absolute
+		top: 15px
+		right: 10px
+		z-index: 2
+	.owl-carousel
+		&__item
+			display: flex
+			align-items: center
+			box-sizing: border-box
+			padding: 20px
+			height: 100vh
+			overflow: hidden
+
+	.mobile-home-main
+		width: 80%
+		margin: 0 auto
+		&__description
+			font-family: 'Raleway'
+			text-align: justify
+			margin-top: 40px
+			line-height: 30px
+		&__text
+			font-family: 'Raleway'
+			opacity: .8
+			&_main
+				margin-top: 50%
+				font-size: 40px
+				letter-spacing: 40px
+				animation: slideRight 3s infinite
+				display: block
+			&_sub
+				display: block
+				margin-top: 10px
+				text-align: right
+				letter-spacing: 5px
+				animation: slideLeft 3s infinite
+
+	.mobile-home-product
+		display: flex
+		justify-content: space-between
+		align-items: center
+		box-sizing: border-box
+		padding: 25px 10px
+		text-decoration: none
+		color: #000
+		border-bottom: 1px solid gray
+		> div
+			width: 48%
+		&__title
+			font-family: 'Ailerons'
+			font-size: 35px
+			opacity: .8
+		&:hover
+			background: red
+
+	@keyframes slideLeft
+		0%
+			transform: translateX(0)
+		50%
+			transform: translateX(10px)
+		100%
+			transform: translateX(0)
+	@keyframes slideRight
+		0%
+			transform: translateX(0)
+		50%
+			transform: translateX(-10px)
+		100%
+			transform: translateX(0)
 </style>
