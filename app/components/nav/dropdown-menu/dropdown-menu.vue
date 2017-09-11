3418f6923a35e8c62daf6bc3c6d12e06a60205dc
v-prusakov <v-prusakov@yandex-team.ru>
2017-09-11 10:52:44 +0300

    menu revamp

--- app/components/nav/dropdown-menu/dropdown-menu.vue
+++ app/components/nav/dropdown-menu/dropdown-menu.vue
@@ -1,10 +1,9 @@
 <template>
-	<div class="dropdown-menu" v-bind:class="wrapperMod" v-on:click="toggleDropdown">
+	<div class="dropdown-menu" v-bind:class="wrapperMod" v-on:click.stop="toggleDropdown">
 		<div class="dropdown-menu__trigger" v-bind:class="mods"></div>
-		<div class="dropdown-menu__content" v-bind:class="{'dropdown-menu__content_active': dropdown}">
-			<ul class="dropdown-menu__list">
+		<div class="dropdown-menu__content" v-on:click.stop="stop" v-bind:class="{'dropdown-menu__content_active': dropdown}">
+			<ul class="dropdown-menu__list" v-on:click="closeDropdown">
 				<li class="dropdown-menu__item"><a href="#/" class="dropdown-menu__link">home</a></li>
-				<li class="dropdown-menu__item"><a href="#/product" class="dropdown-menu__link">product</a></li>
 				<li class="dropdown-menu__item"><a href="#/products" class="dropdown-menu__link">products</a></li>
 			</ul>
 		</div>
@@ -13,12 +12,22 @@
 
 <script>
 import './dropdown-menu.sass';
+const ESCAPE_KEY = 27;
 
 export default {
 	data () {
 		return {
 			dropdown: false
 		}
+	},
+	mounted () {
+		document.addEventListener('keyup', (event) => {
+			if (event.keyCode === ESCAPE_KEY) {
+				this.closeDropdown();
+			}
+		});
+
+		document.addEventListener('click', () => this.closeDropdown());
 	},
 	props: ['dStyle'],
 	computed: {
@@ -36,7 +45,7 @@ export default {
 		}
 	},
 	methods: {
-		toggleDropdown() {
+		toggleDropdown () {
 			this.dropdown = this.dropdown ? false : true;
 
 			if (this.dropdown) {
@@ -44,6 +53,13 @@ export default {
 			} else {
 				$('#app').removeClass('app_shrink');
 			}
+		},
+		stop () {
+			return false;
+		},
+		closeDropdown () {
+			$('#app').removeClass('app_shrink');
+			this.dropdown = false;
 		}
 	}
 }
